e26c89b9bb951bbc5aeaf15693ae84d2544e8b09
Dmitriy Iskra <dmitriyiskra@mail.ru>
2023-03-16 22:50:26 +0300

    added for in in functions

--- src/app/app.js
+++ src/app/app.js
@@ -2,43 +2,57 @@ export const objOrigin = {
   name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
 };
 
-export function sortForAlphabet(obj, finallyObject, filtereddArr) {
-  filtereddArr.forEach((el) => { // Перебираем массив шаблон
-    if (obj?.[el]) { // если в оригинальном объекте есть такие собственные свойства
-      finallyObject.push({ // добавляем в финальный массив соответствующие объекты
-        key: el,
-        value: obj[el],
-      });
+export const arrForSort = ["name", "level"];
+
+
+export function sortForAlphabet(obj, preFinallyObject, resultLastKeys) {
+  
+  resultLastKeys.forEach( el => {
+    for(let key in obj) {
+      if(el === key) {
+        preFinallyObject.push({ // Добавляем в финальный массив строки в заданном порядке
+          key: key,
+          value: obj[key],
+        });
+      }
     }
-  });
-
-  return finallyObject;
+  })
+  
+  return preFinallyObject;
 }
 
-export function filteredKeys(obj, sortRules) {
-  const filtereddArr = Object.keys(obj).filter((e) => e !== sortRules[0] && e !== sortRules[1]); // создаем массив из оставшихся свойств
-  filtereddArr.sort(); // выстраиваем эти свойства в алфавитном порядке
-  return filtereddArr;
+export function filterLastKeys(obj, sortRules) {
+  const arrOfKeys = Object.keys(obj);
+
+  sortRules.forEach( el => arrOfKeys.splice(arrOfKeys.indexOf(el), 1)) // Убираенм лишнее из массива ключей, то что уже добавленно в пред итоговый массив
+  arrOfKeys.sort(); // выстраиваем эти свойства в алфавитном порядке
+
+  return arrOfKeys;
 }
+
+
 
 export function sortForRules(obj, sortRules) {
   let result = null;
-  const preFinallyObject = []; // Создаем финальный массив
+  const preFinallyObject = []; // Создаем пред финальный массив
 
-  const resultKeys = filteredKeys(obj, sortRules); // функция получения оставшихся свойств в алфавитном порядке
 
-  sortRules.forEach((el) => { // Перебираем массив по которому будут выстраиваться первые две строки
-    if (obj?.[el]) {
-      preFinallyObject.push({ // Добавляем в финальный массив строки в заданном порядке
-        key: el,
-        value: obj[el],
-      });
-    }
+  const resultLastKeys = filterLastKeys(obj, sortRules); // функция получения оставшихся свойств в алфавитном порядке
 
-    if (preFinallyObject.length === sortRules.length) { // Если финальный массив той же длинны что и массив с первоначальными установками вызываем функцию
-      result = sortForAlphabet(obj, preFinallyObject, resultKeys); // передаем в функцию отсортированный массив для второй части
-    }
-  });
+  sortRules.forEach( el => { // берем элемент массива сортировки
+    for(let key in obj) { //беребираем ключи объекта оригинала и сравниваем с элементом сортировки
+      if(el === key) { // если находим то добавляем объект в предфинальный массив
+        preFinallyObject.push({
+          key: key,
+          value: obj[key],
+        })
+      }
+
+      if(preFinallyObject.length === sortRules.length) { // Если пред финальный массив той же длинны что и массив с первоначальными установками вызываем функцию
+        result = sortForAlphabet(obj, preFinallyObject, resultLastKeys); // передаем в функцию отсортированный массив для второй части
+      }
+    } 
+  })
 
   return result;
 }
@@ -47,3 +61,6 @@ export default function sortObject(obj, sortRules) { // START
   const result = sortForRules(obj, sortRules);
   return result;
 }
+
+
+console.log(sortObject(objOrigin, arrForSort))
